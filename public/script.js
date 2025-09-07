39ebf976e585e926c7a2d53d25fc9cb43b814383
venkey3413 <venkatesh3413@gmail.com>
2025-09-07 12:36:55 +0530

    Add cancellation policy PDF link below Book Now button

--- public/script.js
+++ public/script.js
@@ -86,7 +86,12 @@ function displayResorts(filteredResorts = resorts) {
                 ${resort.available ? 
                     `<button class="book-btn" onclick="openBookingModal(${resort.id})">
                         <i class="fas fa-calendar-check"></i> Book Now
-                    </button>` : 
+                    </button>
+                    <div class="policy-link">
+                        <a href="cancellation-policy.pdf" target="_blank" class="pdf-link">
+                            <i class="fas fa-file-pdf"></i> Cancellation Policy
+                        </a>
+                    </div>` : 
                     `<button class="book-btn unavailable" disabled>
                         <i class="fas fa-times-circle"></i> Currently Unavailable
                     </button>`
