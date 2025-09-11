4985032fb53dde50d11653f19c6b23cf45b8f2be
venkey3413 <venkatesh3413@gmail.com>
2025-09-11 22:11:53 +0530

    fix: Add dotenv config to backup service

--- backup-service.js
+++ backup-service.js
@@ -2,6 +2,8 @@ const AWS = require('aws-sdk');
 const fs = require('fs');
 const cron = require('node-cron');
 
+require('dotenv').config();
+
 const s3 = new AWS.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
