4aabb856357cc506d66e6c2cc42a4bbb5bc3463c
venkey3413 <venkatesh3413@gmail.com>
2025-09-16 22:03:27 +0530

    Fix nodemailer function name

--- email-service.js
+++ email-service.js
@@ -2,7 +2,7 @@ const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 // Create transporter
-const transporter = nodemailer.createTransporter({
+const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
         user: process.env.EMAIL_USER,
