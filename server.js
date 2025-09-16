0c6220a32c4603b6ca7a0faf2efc2ede7a64432f
venkey3413 <venkatesh3413@gmail.com>
2025-09-16 18:48:06 +0530

    Fix port issue - use PORT env variable or default 3000

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { generatePaymentDetails } = require('./upi-service');
 const path = require('path');
 
 const app = express();
-const PORT = 80;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
