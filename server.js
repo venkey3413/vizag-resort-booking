79e4ea931a0d467abe153b4aae70cbcc25315892
venkey3413 <venkatesh3413@gmail.com>
2026-02-17 13:18:46 +0530

    Change domain from vizagresortbooking.in to vshakago.in

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ function sanitizeInput(input) {
 
 // Middleware
 app.use(cors({
-    origin: process.env.NODE_ENV === 'production' ? ['https://vizagresortbooking.in'] : true,
+    origin: process.env.NODE_ENV === 'production' ? ['https://vshakago.in'] : true,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
